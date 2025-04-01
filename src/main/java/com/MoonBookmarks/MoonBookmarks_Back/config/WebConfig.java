8205c4a58b331ced8d5476b4aa76d9d6a756c862
githuParaLab3 <leshenlier@gmail.com>
2025-04-01 00:35:19 -0300

    Segundo Commit

---
+++ src/main/java/com/MoonBookmarks/MoonBookmarks_Back/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.MoonBookmarks.MoonBookmarks_Back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081") // Endereço do seu frontend (ajuste conforme necessário)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
