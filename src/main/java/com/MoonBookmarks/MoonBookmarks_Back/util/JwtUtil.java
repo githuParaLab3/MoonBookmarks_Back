b3a0c21b7e0279f29eab5d08790813dbaa69358f
githuParaLab3 <leshenlier@gmail.com>
2025-04-03 21:59:13 -0300

    13

--- src/main/java/com/MoonBookmarks/MoonBookmarks_Back/util/JwtUtil.java
+++ src/main/java/com/MoonBookmarks/MoonBookmarks_Back/util/JwtUtil.java
@@ -2,21 +2,26 @@ package com.MoonBookmarks.MoonBookmarks_Back.util;
 
 import java.util.Date;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
-    private final String SECRET_KEY = "superSecretKey"; // Troque por algo mais seguro
+    // Geração da chave secreta segura
+    private final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 dia de validade
+                .signWith(SECRET_KEY)
                 .compact();
     }
 
@@ -25,8 +30,9 @@ public class JwtUtil {
     }
 
     private Claims getClaims(String token) {
-        return Jwts.parser()
+        return Jwts.parserBuilder()
                 .setSigningKey(SECRET_KEY)
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
