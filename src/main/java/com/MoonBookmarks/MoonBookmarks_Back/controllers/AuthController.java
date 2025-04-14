f292afe93baff794ac508b9a1b2450f3350ed11e
githuParaLab3 <leshenlier@gmail.com>
2025-04-14 01:01:43 -0300

    34

--- src/main/java/com/MoonBookmarks/MoonBookmarks_Back/controllers/AuthController.java
+++ src/main/java/com/MoonBookmarks/MoonBookmarks_Back/controllers/AuthController.java
@@ -15,6 +15,8 @@ import com.MoonBookmarks.MoonBookmarks_Back.dto.AuthRequest;
 import com.MoonBookmarks.MoonBookmarks_Back.entities.Usuario;
 import com.MoonBookmarks.MoonBookmarks_Back.repositories.UsuarioRepository;
 import com.MoonBookmarks.MoonBookmarks_Back.util.JwtUtil;
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/auth")
@@ -30,23 +32,30 @@ public class AuthController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody AuthRequest request) {
+    public ResponseEntity<Map<String, String>> login(@RequestBody AuthRequest request) {
         // Verifica se o email ou senha estão vazios
         if (request.getEmail() == null || request.getEmail().isEmpty() || request.getSenha() == null || request.getSenha().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email ou senha não podem ser vazios");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", "Email ou senha não podem ser vazios"));
         }
-
+    
         Optional<Usuario> usuarioOpt = usuarioRepository.findByEmail(request.getEmail());
-
+    
         // Verifica se o usuário existe e a senha corresponde
         if (usuarioOpt.isPresent() && passwordEncoder.matches(request.getSenha(), usuarioOpt.get().getSenha())) {
             // Gera o token JWT
             String token = jwtUtil.generateToken(request.getEmail());
-            return ResponseEntity.ok(token); // Retorna o token gerado
+    
+            // Retorna o token e o id do usuário em um Map
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+            response.put("userId", usuarioOpt.get().getId().toString()); // Incluindo o id do usuário na resposta
+    
+            return ResponseEntity.ok(response); // Retorna o token e o id do usuário
         }
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "Credenciais inválidas"));
     }
+
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody Usuario usuario) {
